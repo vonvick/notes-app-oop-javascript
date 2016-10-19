c06221339f6bc2a41b1cfd1abc6f1635a04dfa67
vonvick <victor.nwaiwu9@gmail.com>
2016-10-19 18:14:47 +0100

    create a Human class that extends from the Animal class.

--- notes-app.js
+++ notes-app.js
@@ -1,21 +1,6 @@
 /**
- * Class Note that creates the note.
- * @param {string} author the author name of the note.
- * @param {string} note_content The note to be stored in the list of notes.
- * @constructor
- */
-class Note {
-  // constructor for Note Class
-  constructor(author, note_content) {
-    this.author = author;
-    this.note = note_content;
-  }
-}
-
-/**
  * Class NoteApplication that creates the note application.
  * @param {string} author the author name of the note.
- * @extends {Note}
  * @constructor
  */
 class NotesApplication {
@@ -27,15 +12,14 @@ class NotesApplication {
   
   // The function that creates a note
   create(note_content) {
-  	var newNote = new Note (name, note_content);
-    return this.notes.push(newNote.note);
+    return this.notes.push(note_content);
   }
 
   // The function that lists the notes for an author
   listNotes() {
-    var listResults = "";
+    var listResults = '';
     if (this.notes.length < 1) {
-      listResults += "You do not have any note here!";
+      listResults += 'You do not have any note here!';
       return listResults;
     } else {
       for (var i = 0; i < this.notes.length ; i++) {
@@ -49,7 +33,7 @@ class NotesApplication {
 
   // The function that gets a note by the ID
   getNotes(note_id) {
-    var getResult = "";
+    var getResult = '';
     if (typeof note_id === 'number') {
       var note = this.notes[note_id - 1];
       if (note) {
@@ -61,17 +45,17 @@ class NotesApplication {
         return getResult;
       }
     }
-    return "You did not enter a number";
+    return 'You did not enter a number';
   }
 
   // function that searches for a keyword in the list of Notes
   search(search_text) {
     console.log('Showing results for search: ' + search_text + '\n');
-    var searchResult = "";
+    var searchResult = '';
     var searchCount = 0;
     var author = this.name;
-    if (search_text === "" || search_text === " ") {
-    	return "You did not enter a search text";
+    if (search_text === '' || search_text === ' ') {
+    	return 'You did not enter a search text';
     } else {
       for (var i = 0; i < this.notes.length; i++) {
    	    if (this.notes[i].indexOf(search_text) !== -1) {
@@ -91,7 +75,7 @@ class NotesApplication {
 
   // function that deletes a note from the note Lists
   delete(note_id) {
-    var deleted = "";
+    var deleted = '';
     if (this.notes.length < note_id - 1) {
       deleted += 'Could not find the file to delete';
       return deleted;
@@ -103,7 +87,7 @@ class NotesApplication {
   }
 
   edit(note_id, new_content) {
-    var editResult = "";
+    var editResult = '';
     if (this.notes.length < note_id - 1) {
       editResult += 'Could not find the file to edit';
       return editResult;
--- oop-app.js
+++ oop-app.js
@@ -20,3 +20,25 @@ class Animal {
   }
 }
 
+/**
+ * Class Human that creates the Human.
+ * @param {string} sound the sound the animal makes.
+ * @param {integer} legs the number of leg of the animal
+ * @constructor
+ */
+
+class Human extends Animal {
+  constructor(sound, legs, name) {
+    super(sound,legs);
+    this.name = name;
+  }
+
+  speak() {
+    var talks = this.name + 'speaks' + this.sound;
+    return talks
+  }
+
+  hasHowManyLegs() {
+    return this.legs
+  }
+}
